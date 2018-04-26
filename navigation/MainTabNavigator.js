48952fd8e8155b4420a792a58ae2758a6145cded
Joh Yoshida <johyoshida@gmail.com>
2018-04-25 21:59:22 -0700

    Screens to build out

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,20 +6,35 @@ import { TabNavigator, TabBarBottom } from 'react-navigation';
 import Colors from '../constants/Colors';
 
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-
+// import LinksScreen from '../screens/LinksScreen';
+// import SettingsScreen from '../screens/SettingsScreen';
+import QuotesScreen from '../screens/QuotesScreen';
+import JournalScreen from '../screens/JournalScreen';
+import HabitsScreen from '../screens/HabitsScreen';
+import IdeasScreen from '../screens/IdeasScreen';
 
 export default TabNavigator(
   {
     Home: {
       screen: HomeScreen,
     },
-    Links: {
-      screen: LinksScreen,
+    // Links: {
+    //   screen: LinksScreen,
+    // },
+    // Settings: {
+    //   screen: SettingsScreen,
+    // },
+    Quotes: {
+      screen: QuotesScreen,
     },
-    Settings: {
-      screen: SettingsScreen,
+    Journal: {
+      screen: JournalScreen,
+    },
+    Habits: {
+      screen: HabitsScreen,
+    },
+    Ideas: {
+      screen: IdeasScreen,
     }
   },
   {
@@ -40,6 +55,22 @@ export default TabNavigator(
           case 'Settings':
             iconName =
               Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+              break;
+          case 'Quotes':
+            iconName =
+              Platform.OS === 'ios' ? `ios-egg${focused ? '' : '-outline'}` : 'md-egg';
+              break;
+          case 'Journal':
+            iconName =
+              Platform.OS === 'ios' ? `ios-book${focused ? '' : '-outline'}` : 'md-book';
+              break;
+          case 'Habits':
+            iconName =
+              Platform.OS === 'ios' ? `ios-cafe${focused ? '' : '-outline'}` : 'md-cafe';
+            break;
+          case 'Ideas':
+            iconName =
+              Platform.OS === 'ios' ? `ios-bulb${focused ? '' : '-outline'}` : 'md-bulb';
         }
         return (
           <Ionicons
