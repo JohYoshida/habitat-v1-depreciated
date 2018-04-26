1bcd50faed4e044517c449c07e9b36269be62099
Joh Yoshida <johyoshida@gmail.com>
2018-04-25 21:58:14 -0700

    Basic screens

---
+++ screens/HabitsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class HabitsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Habits',
+  };
+
+  render() {
+    return (
+      <View>
+      </View>
+    )
+  }
+}
---
+++ screens/JournalScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class JournalScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Journal',
+  };
+  
+  render() {
+    return (
+      <View>
+      </View>
+    )
+  }
+}
