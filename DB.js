75bb07b76831e5ed0d387b63f4cc15a883013b67
Joh Yoshida <johyoshida@gmail.com>
2018-04-30 11:47:50 -0700

    Create habits model in DB

--- DB.js
+++ DB.js
@@ -3,6 +3,7 @@ var RNDBModel = require('react-native-db-models')
 var DB = {
     "quotes": new RNDBModel.create_db("quotes"),
     "journalEntry": new RNDBModel.create_db("journalEntry"),
+    "habits": new RNDBModel.create_db("habits"),
 }
 
 module.exports = DB
